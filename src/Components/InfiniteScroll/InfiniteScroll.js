5d03b41d6da46e465d4d8d1959aa5923ff34eb55
sechs <philippe.buteau@skema.edu>
2021-07-07 17:34:13 +0200

    search implemented

--- src/Components/InfiniteScroll/InfiniteScroll.js
+++ src/Components/InfiniteScroll/InfiniteScroll.js
@@ -7,6 +7,7 @@ export default function InfiniteScroll() {
   const [dataImg, setDataImg] = useState([[], [], []]);
   const [pageIndex, setPageIndex] = useState(1);
   const [searchState, setSearchState] = useState("random");
+  const [firstCall, setFirstCall] = useState("true");
 
   const infiniteFetchData = () => {
     fetch(
@@ -38,10 +39,45 @@ export default function InfiniteScroll() {
           }
         }
         setDataImg(newFreshState);
+        setFirstCall(false);
       });
   };
 
-  console.log(dataImg);
+  const searchFetchData = () => {
+    fetch(
+      `https://api.unsplash.com/search/photos?page=${pageIndex}&per_page=30&query=${searchState}&client_id=TccZKDXp9rzfJUM7avYkdoikF-6NFDgdYhvZF5HiunQ`
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        // console.log(data);
+
+        const imgsReceived = [];
+        data.results.forEach((img) => {
+          imgsReceived.push(img.urls.regular);
+        });
+
+        const newFreshState = [[], [], []];
+
+        let index = 0;
+
+        for (let i = 0; i < 3; i++) {
+          for (let j = 0; j < 10; j++) {
+            newFreshState[i].push(imgsReceived[index]);
+            index++;
+          }
+        }
+        setDataImg(newFreshState);
+      });
+  };
+
+  useEffect(() => {
+    if (firstCall) return;
+    searchFetchData();
+  }, [searchState]);
+
+  //   console.log(dataImg);
 
   useEffect(() => {
     infiniteFetchData();
@@ -49,6 +85,9 @@ export default function InfiniteScroll() {
 
   const handleSearch = (e) => {
     e.preventDefault();
+
+    setSearchState(inpRef.current.value);
+    setPageIndex(1);
   };
 
   const inpRef = useRef();
